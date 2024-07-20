6bc95ef57e39ccb6ecf022ab52dac4722a4db3ea
Vageshcoc <121989671+Vageshcoc@users.noreply.github.com>
2024-07-21 04:24:45 +0530

    Update PlayerContext.jsx

--- spotify-clone/src/context/PlayerContext.jsx
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -9,7 +9,7 @@ const PlayerContextProvider = (props) => {
   const seekBg = useRef();
   const seekBar = useRef();
 
-  const url = 'http://localhost:4000';
+  const url = 'https://spotify-backend-nazg.onrender.com';
 
 
   const [songsData,setSongsData] = useState([]);
